0220e23db26935419a5b55d374937b52953bd7ee
sattsat <syuuatit141offer@gmail.com>
2024-12-03 01:25:21 +0900

    ホーム画面のレイアウトを構成

--- RestaurantSearchApp/Views/HomeView.swift
+++ RestaurantSearchApp/Views/HomeView.swift
@@ -7,13 +7,79 @@
 //
 
 import SwiftUI
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static let tokyoStation = CLLocationCoordinate2D(latitude: 35.681271031625556, longitude: 139.76710334029735)
+}
+
 
 struct HomeView: View {
     
     @EnvironmentObject var manager: LocationManager
     
+    @State private var position: MapCameraPosition =
+        .userLocation(fallback: .automatic)
+    @State private var isInteracting = false
+    @State private var searchText: String = ""
+    @FocusState var isFocus: Bool
+    
     var body: some View {
-        Text("ホームです")
+        ZStack {
+            if isFocus {
+                Color.gray.edgesIgnoringSafeArea(.all)
+            } else {
+                mapView
+                    .onAppear {
+                        position = .userLocation(fallback: .automatic)
+                    }
+            }
+            
+            VStack {
+                SearchBar(searchText: $searchText)
+                    .opacity(isInteracting ? 0.2 : 1)
+                    .focused($isFocus)
+                    .submitLabel(.search)
+                
+                Spacer()
+            }
+            .padding()
+        }
+    }
+    
+    private var mapView: some View {
+        Map(position: $position) {
+            UserAnnotation()
+        }
+        .tint(.orange)
+        .onMapCameraChange(frequency: .continuous) {
+            if !position.followsUserLocation && !isInteracting {
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    isInteracting = true
+                }
+            }
+        }
+        .onMapCameraChange(frequency: .onEnd) {
+            if isInteracting && !position.followsUserLocation {
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    isInteracting = false
+                }
+            }
+        }
+        .overlay(alignment: .bottomTrailing) {
+            Button(action: {
+                position = .userLocation(fallback: .automatic)
+            }) {
+                Image(systemName: "location.fill")
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .clipShape(Circle())
+                    .shadow(radius: 5)
+            }
+            .opacity(isInteracting ? 0.2 : 1)
+            .padding(.trailing, 16)
+            .padding(.bottom, 16)
+        }
     }
 }
 
