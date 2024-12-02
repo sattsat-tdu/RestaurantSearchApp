0888746cdcf9a0ca1063838c91fec0872cc2e90a
sattsat <syuuatit141offer@gmail.com>
2024-12-02 10:53:25 +0900

    app initial commit

---
+++ RestaurantSearchApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  RestaurantSearchApp
+//  
+//  Created by SATTSAT on 2024/12/02
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RestaurantSearchApp/RestaurantSearchAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantSearchAppApp.swift
+//  RestaurantSearchApp
+//  
+//  Created by SATTSAT on 2024/12/02
+//  
+//
+
+import SwiftUI
+
+@main
+struct RestaurantSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
