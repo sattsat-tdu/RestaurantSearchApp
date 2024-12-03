a99c265e260e9cdf4be02e344547dcc9c1996c77
sattsat <syuuatit141offer@gmail.com>
2024-12-04 00:02:29 +0900

    APIとの送受信を行うmanagerをシングルトンで作成

---
+++ RestaurantSearchApp/Managers/GourmetAPIManager.swift
@@ -0,0 +1,12 @@
+//
+//  GourmetAPIManager.swift
+//  RestaurantSearchApp
+//  
+//  Created by SATTSAT on 2024/12/03
+//  
+//
+
+class GourmetAPIManager {
+    //シングルトン
+    static let shared = GourmetAPIManager()
+}
