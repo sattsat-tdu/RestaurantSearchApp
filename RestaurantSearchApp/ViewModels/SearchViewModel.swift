b8ba61819a2dcdd2721672979abd233fb108b3fd
sattsat <syuuatit141offer@gmail.com>
2024-12-04 09:26:49 +0900

    検索画面との通信、マネージャーとの通信の橋渡しをするViewModelを作成、確認処理を実装

--- RestaurantSearchApp/ViewModels/SearchViewModel.swift
+++ RestaurantSearchApp/ViewModels/SearchViewModel.swift
@@ -8,7 +8,42 @@
 
 import SwiftUI
 
-@MainActor
+enum SearchRange: Int, CaseIterable {
+    case d300 = 1
+    case d500 = 2
+    case d1000 = 3
+    case d2000 = 4
+    case d3000 = 5
+    
+    /// 距離 (m) を取得
+    var toMeters: Int {
+        switch self {
+        case .d300: return 300
+        case .d500: return 500
+        case .d1000: return 1000
+        case .d2000: return 2000
+        case .d3000: return 3000
+        }
+    }
+}
+
 final class SearchViewModel: ObservableObject {
     
+    @Published var searchText: String = ""
+    //range設定
+    @Published var selectedRange: SearchRange = .d1000
+    
+    @MainActor
+    func fetchRestauarnats() {
+        Task {
+            let result = await GourmetAPIManager.shared.fetchRestaurants()
+            switch result {
+            case .success(let success):
+                print("成功!")
+                print(success)
+            case .failure(let failure):
+                print("失敗！\(failure.localizedDescription)")
+            }
+        }
+    }
 }
